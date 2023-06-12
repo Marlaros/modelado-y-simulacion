cfd2c79223d77c25972ab1a83b26ccc818f64d2f
Tobias Kleppe <tkleppe@uade.edu.ar>
2023-06-12 01:52:18 -0300

    General Layout + Methods

---
+++ app/components/Montecarlo.tsx
@@ -0,0 +1,69 @@
+import React, {useState} from 'react'
+
+const Montecarlo = () => {
+    const [parametros, setParametros] = useState({
+        "funcion": "",
+        "puntos": "",
+        "inicio": "",
+        "fin": ""
+    })
+
+    return(
+        <div className="flex flex-col justify-center items-center min-h-50 bg-blue-400 m-6" style={{border: 'solid 4px', borderRadius: '8px 4px 8px 4px'}}>
+            <div className="flex-2 text-3xl m-4 font-bold">
+                Método Montecarlo
+            </div>
+            <div className="flex-1 flex flex-row flex-wrap justify-center text-2xl m-10 text-2lg">
+                <div className="flex flex-row justify-between min-w-50 m-3">
+                    <label className="m-1" htmlFor="funcion">Función: {'f(x) '}</label>
+                    <input 
+                        type="text"
+                        name="funcion"
+                        className="m-1 px-2 bg-blue-200"
+                        style={{border: 'solid 2px', borderRadius: '4px 3px 4px 3px'}}
+                        value={parametros.funcion}
+                        onChange={(e) => setParametros((prevState) => ({...prevState, funcion: e.target.value}))}
+                    />
+                </div>
+                <div className="flex flex-row justify-between min-w-50 m-3">
+                    <label className="m-1" htmlFor="intervalos">Numero de Puntos: {'n '}</label>
+                    <input 
+                        type="text"
+                        name="funcion"
+                        className="m-1 px-2 bg-blue-200"
+                        style={{border: 'solid 2px', borderRadius: '4px 3px 4px 3px'}}
+                        value={parametros.puntos}
+                        onChange={(e) => setParametros((prevState) => ({...prevState, puntos: e.target.value}))}
+                    />
+                </div>
+                <div className="flex flex-row justify-between min-w-50 m-3">
+                    <label className="m-1" htmlFor="inicio">Inicio: {'a '}</label>
+                    <input 
+                        type="text"
+                        name="funcion"
+                        className="m-1 px-2 bg-blue-200"
+                        style={{border: 'solid 2px', borderRadius: '4px 3px 4px 3px'}}
+                        value={parametros.inicio}
+                        onChange={(e) => setParametros((prevState) => ({...prevState, inicio: e.target.value}))}
+                    />
+                </div>
+                <div className="flex flex-row justify-between min-w-50 m-3">
+                    <label className="m-1" htmlFor="fin">Fin: {'b '}</label>
+                    <input 
+                        type="text"
+                        name="funcion"
+                        className="m-1 px-2 bg-blue-200"
+                        style={{border: 'solid 2px', borderRadius: '4px 3px 4px 3px'}}
+                        value={parametros.fin}
+                        onChange={(e) => setParametros((prevState) => ({...prevState, fin: e.target.value}))}
+                    />
+                </div>
+            </div>
+            <div className="flex-2 text-2xl m-4 font-bold">
+                <button style={{border: 'solid 2px', borderRadius: '8px 4px 8px 4px'}} className="px-6 m-4 bg-teal-400 hover:bg-teal-300 hover:font-black" >Calcular</button>
+            </div>
+        </div>
+    )
+}
+
+export default Montecarlo;
---
+++ app/page.tsx
@@ -0,0 +1,77 @@
+"use client"
+import React, {useState} from "react";
+import Rectangulos from "./components/Rectangulos";
+import Trapecios from "./components/Trapecios";
+import Simpsons from "./components/Simpsons";
+import Montecarlo from "./components/Montecarlo";
+
+enum Method{
+  rectangulos = "Rectángulos",
+  trapecios = "Trapecios",
+  simpsons = "Simpsons",
+  montecarlo = "Montecarlo"
+}
+
+export default function Home() {
+
+  const [method, setMethod] = useState(Method.rectangulos);
+
+  const renderMethod = () => {
+    switch(method){
+      case Method.rectangulos:
+          return <Rectangulos />
+      case Method.trapecios:
+          return <Trapecios />
+      case Method.simpsons:
+          return <Simpsons />
+      case Method.montecarlo:
+          return <Montecarlo />
+      default:
+          return <>Some method</>
+    }
+}
+
+  return (
+    <main className="flex flex-col items-center justify-between p-24 bg-teal-100 min-h-screen">
+      <div className="flex-2 font-bold font-black text-4xl ">
+        Modelado y Simulación
+      </div>
+      <div className="m-6 flex-2 font-bold font-black text-2xl ">
+        Cáclulo de Integrales - Métodos de Aproximación
+      </div>
+      <div className="m-4 flex-2 flex flex-row items-center justify-center" >
+        <button 
+          className="m-6 px-20 bg-teal-400 hover:bg-teal-200 font-bold h-10 text-2xl"
+          onClick={() => setMethod(Method.rectangulos)}
+          style={{boxShadow: '2px 3px 2px 3px rgba(0,0,0,0.11)', borderRadius: '4%', minHeight: '50px'}}
+        >
+          Rectángulos
+        </button>
+        <button  
+          className="m-6 px-20 bg-teal-400 hover:bg-teal-200 font-bold h-10 text-2xl"
+          onClick={() => setMethod(Method.trapecios)}
+          style={{boxShadow: '2px 3px 2px 3px rgba(0,0,0,0.11)', borderRadius: '4%', minHeight: '50px'}}
+        >
+          Trapecios
+        </button>
+        <button  
+          className="m-6 px-20 bg-teal-400 hover:bg-teal-200 font-bold h-10 text-2xl"
+          onClick={() => setMethod(Method.simpsons)}
+          style={{boxShadow: '2px 3px 2px 3px rgba(0,0,0,0.11)', borderRadius: '4%', minHeight: '50px'}}
+        >
+          Simpsons
+        </button>
+        <button  
+          className="m-6 px-20 bg-teal-400 hover:bg-teal-200 font-bold h-10 text-2xl"
+          onClick={() => setMethod(Method.montecarlo)}
+          style={{boxShadow: '2px 3px 2px 3px rgba(0,0,0,0.11)', borderRadius: '4%', minHeight: '50px'}}
+        >
+          Montecarlo
+        </button>
+      </div>
+      <div className="flex-1 w-full max-w-5xl items-center justify-between font-mono text-md divide-black">
+        {renderMethod()}
+      </div>
+    </main>
+  )
+}
